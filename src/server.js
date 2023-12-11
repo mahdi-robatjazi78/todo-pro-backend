f35ad496acb04c2f80f5ff8a3758e9eb574342fc
m.robatjazi <43264216+mahdi-robatjazi78@users.noreply.github.com>
2023-12-11 21:33:34 +0330

    cors change test for server request

--- src/server.js
+++ src/server.js
@@ -4,13 +4,17 @@ const MainRoutes = require("./routes")
 const cors = require("cors")
 const path = require("path")
 
-// const corsOptions = {
-//     origin: 'http://localhost:3000', // Replace with your client's URL
-//     methods: ['GET', 'POST' , 'PUT' , "DELETE"],
-//     allowedHeaders: ['Content-Type', 'X-Auth-Token'],
-// };
+const corsOptions = {
+    // origin: 'http://localhost:3000', // Replace with your client's URL
+    // methods: ['GET', 'POST' , 'PUT' , "DELETE"],
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ['Content-Type', 'X-Auth-Token'],
+};
 
-const corsOptions = "*"
+// const corsOptions = "*"
 
 app.use(express.static("public"))
 app.use(cors(corsOptions))
